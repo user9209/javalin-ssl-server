9d8056e4f1cfa8a8be2a84dd1a3a7798d295e5ef
Georg Schmidt <development@gs-sec.de>
2022-11-23 20:04:16 +0100

    prevent information leakage

--- src/main/java/de/cryptosdk/javalin/ssl/server/context/SslContextForJavalin.java
+++ src/main/java/de/cryptosdk/javalin/ssl/server/context/SslContextForJavalin.java
@@ -1,11 +1,21 @@
 package de.cryptosdk.javalin.ssl.server.context;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.util.BufferUtil;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -15,6 +25,8 @@ public class SslContextForJavalin {
 
     private static final String pkcs12Keyfile = "localhost.p12";
     private static final String pkcs12Password = "1234";
+
+    private static final Logger LOG = LoggerFactory.getLogger(ErrorHandler.class);
 
     public static Server getServer(int portHttp, int portHttps, boolean tls_13_only) {
         Server server = new Server();
@@ -38,6 +50,7 @@ public class SslContextForJavalin {
         } else {
             server.setConnectors(new Connector[]{sslConnector, connector});
         }
+        server.setErrorHandler(new FixedErrorHandler());
         return server;
     }
 
@@ -76,5 +89,25 @@ public class SslContextForJavalin {
             );
         }
         return sslContextFactory;
+    }
+
+    /*
+        Handles error: ip or domain is called without valid cert for SNI
+     */
+    private static class FixedErrorHandler extends ErrorHandler {
+        @Override
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+            LOG.warn("FixedErrorHandler for '" + baseRequest.getLocalName() + baseRequest.getOriginalURI() + "'");
+            response.setStatus(500);
+            response.setHeader("server","???");
+            response.getWriter().println("<h1>Internal Server Error 500</h1>");
+        }
+
+        @Override
+        public ByteBuffer badMessageError(int status, String reason, HttpFields.Mutable fields)
+        {
+            LOG.warn("BadMessageError " + status + " '" + reason + "'");
+            return BufferUtil.toBuffer("<h1>Bad Message " + status + "</h1><pre>reason see logs!</pre>");
+        }
     }
 }
